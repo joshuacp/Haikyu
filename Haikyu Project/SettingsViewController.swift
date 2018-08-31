4b7331f561b6940c64dcf7a91abc0e15e56c4233
Joshua Patrick <jcpatrick@verisk.com>
2018-08-30 20:45:16 -0600

    Tab bar controller with Settings icon

---
+++ Haikyu Project/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Haikyu Project
+//
+//  Created by Joshua Patrick on 8/30/18.
+//  Copyright © 2018 xactimate28. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
